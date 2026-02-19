8989d4639a82cef5f6b95c430975d38deaf9b49f
48-create <mullerkonstantin48@gmail.com>
2026-02-19 16:23:06 +0100

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -89,10 +89,12 @@ end
 -- Buttons erstellen
 CreateButton("AK Admin", "https://absent.wtf/AKADMIN.lua")
 CreateButton("talentPiano", "https://hellohellohell0.com/talentless-raw/TALENTLESS.lua")
+CreateButton("infinity", "https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source")
 CreateButton("Fly GIG", "https://raw.githubusercontent.com/user2345600/FlyGiG/main/FlyGiG.lua")
 CreateButton("DrivingEmp", "https://raw.githubusercontent.com/Marco8642/science/main/drivingempire")
 CreateButton("Auto TP", "https://raw.githubusercontent.com/48-create/CordGIG/refs/heads/main/maincode.lua")
 CreateButton("MillitäryT", "https://gist.githubusercontent.com/1diamondpro1/3ed16f3c81f74aede9a895dcd1fd4ba4/raw/fa95332a2d6c2045b3d18f2018ae998955856b1d/gistfile1.txt")
+CreateButton("MillitäryT2", "https://raw.githubusercontent.com/MortyMo22/roblox-scripts/refs/heads/main/MilitaryTycoon")
 
 -- K-Taste Toggle
 UIS.InputBegan:Connect(function(input, gameProcessed)
